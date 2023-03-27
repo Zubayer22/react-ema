485dd7e46b0198f611727f48c979a390898a0d47
zubayer22 <zubayerkh@gmail.com>
2023-03-27 17:21:01 +0600

    shop start

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h1>Shop</h1>
+            </div>
+            <div className='cart-container'>
+                <h3>Order Summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
